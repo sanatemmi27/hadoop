3d7dbe246388915cd7219399bd9be3e9259a571d
sanatemmi27 <49674024+sanatemmi27@users.noreply.github.com>
2019-04-22 04:27:15 +0530

    Delete new_traffic1.py

--- new_traffic1.py
+++
@@ -1,6 +0,0 @@
-@outputSchema("hi_out:{(year:float,miles:float,vans:float,trucks:float)}")
-
-def hi_miles(traff):
-	year,kms,vans,trucks=traff.split(' ')
-	miles = float(kms)*0.62
-	return year,miles,vans,trucks
